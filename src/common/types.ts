34f62d31a28f0d7a44f9ba5c9b8f8e6c01c20c01
dgarciacasam <dgarciacasam@gmail.com>
2024-08-29 20:35:41 +0200

    feat: Componetizado ProjectMenu

--- src/common/types.ts
+++ src/common/types.ts
@@ -28,10 +28,26 @@ export interface ContentProps {
   projects: Project[]
   setProjects: (newProjects: Project[]) => void
   selectedProjectId: number
+  isShown: boolean
 }
 
 export interface SecondarySidenavProps {
   projects: Project[]
   selectedProjectId: number
   setSelectedProject: (selectedProjectId: number) => void
+  isShown: boolean
+  setIsShown: () => void
+}
+
+export interface ProjectMenuProps {
+  projectView: views
+  setProjectView: (view: views) => void
+  projects: Project[]
+  setProjects: (newProjects: Project[]) => void
+  selectedProject: Project | undefined
+}
+
+export enum views {
+  board = 'board',
+  table = 'table',
 }
